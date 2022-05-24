5df1832cac011c0d6cb0888d00a1759510296546
Tamiriam Fernandes <tamiriam.fernandes@copastur.com.br>
2022-05-23 23:34:46 -0300

    Criado metodo listar todos e buscar por id utilizando as boas praticas de retorno.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,39 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("filme")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperarFilmesById), new { Id = filme.Id}, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarFilmesById(int id)
+        {
+            var filme = filmes.FirstOrDefault(f => f.Id == id);
+            if(filme != null)
+                return Ok(filme);
+
+            return NotFound();
+        }
+    }
+}
